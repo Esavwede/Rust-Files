f6b6fdc31bb6af830a0a95481cf8ab50bcf21823
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-05-02 14:44:38 +0100

    feat: added variables

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 fn main() {
-    println!("Hello, world!");
-    println!("How are you doing"); 
-
-    let name: &str = "Grace";
-    let age: i32 = 24;
-
-
-    println!("{} is {} years old", name, age );
-
+    variables();
 }
 
-// This is a program in Rust Written By
-/*
-    Name: Ogagaoghene Esavwede
-    Email: Ogaga.tech
-*/
 
+
+fn variables()
+{
+    // String 
+    let name = "Hanna";
+    println!("My name is {}",name); 
+
+    // Integers 
+    let age: i32 = 24; 
+    let special_number: i8 = 126;
+
+    println!("My age is {}",age); 
+    println!("My special number is {}",special_number);
+
+    // Floats 
+    let weight: f32 = 101.7; 
+
+    println!("I weigh {} pounds",weight);
+
+    // Char
+    let first_letter_of_name: char = 'H';
+
+    println!("The first letter of my name is {}",first_letter_of_name);
+
+    // Boolean
+    let active_status: bool = true;
+
+    println!("My active status is: {}", active_status);
+    
+}
