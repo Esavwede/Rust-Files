31968028128baec525eac2479e1cee9aeacdea18
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-05-02 15:03:40 +0100

    feat: added arithmetic operations, if else

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+
+
 fn main() {
     variables();
+    operations();
+    if_else();
+    matching();
 }
-
 
 
 fn variables()
@@ -32,4 +36,47 @@ fn variables()
 
     println!("My active status is: {}", active_status);
     
+} 
+
+
+fn operations()
+{
+    arithmetic();
+}
+
+fn arithmetic()
+{
+    println!(" 1 * 1 = {}", 1 * 1 );
+    println!("1 + 1 = {}",1 + 1 );
+    println!(" 1 / 1 = {}",1 / 1 );
+    println!(" 1 - 1 = {}", 1 - 1 );
+    println!("10 % 5 = {}", 10 % 5 ); 
+}
+
+
+fn if_else()
+{
+    let user_online: bool = false; 
+
+    if !user_online 
+    {
+        println!("User is not online ")
+    }
+    else {
+        print!("User is online ")
+    }
+}
+
+fn matching()
+{
+    let number: i8 = 4; 
+
+    match number {
+
+     4 => print!("You have inputted the value {}",number),
+     _ => {
+        print!("Unknown Number inputted"); 
+          println!("Please try again")
+     }
+    }
 }
